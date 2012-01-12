eb912bb559cf6b2c3bc18a4b390dd6f3b77c8bb0
Andrew Havens <andrew.havens@evanta.com>
2012-01-12 14:55:36 -0800

    fixed a bug where only defined links were getting target=blank

--- lib/mad_chatter/markdown_renderer.rb
+++ lib/mad_chatter/markdown_renderer.rb
@@ -3,5 +3,8 @@ module MadChatter
     def link(link, title, alt_text)
       "<a target=\"_blank\" href=\"#{link}\">#{alt_text}</a>"
     end
+    def autolink(link, link_type)
+      "<a target=\"_blank\" href=\"#{link}\">#{link}</a>"
+    end
   end
 end
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -23,9 +23,14 @@ describe MadChatter::Message do
       message.filter.should == ''
     end
     
-    it 'should set links to target="_blank"' do
+    it 'should add target="_blank" to links' do
       message = MadChatter::Message.new('message', "[link me up](http://www.example.com)")
       message.filter.should match %r{<a target="_blank" href="http://www.example.com">link me up</a>}
+    end
+    
+    it 'should also add target="_blank" to links that were autolinked' do
+      message = MadChatter::Message.new('message', 'http://www.example.com')
+      message.filter.should match %r{<a target="_blank" href="http://www.example.com">http://www.example.com</a>}
     end
   end
 
