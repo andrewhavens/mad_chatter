9321eb181611cba619595d58aa6746b7b5663205
Andrew Havens <andrew.havens@evanta.com>
2012-02-08 17:34:55 -0800

    updated a few things

--- templates/extensions.rb
+++ templates/extensions.rb
@@ -12,18 +12,20 @@
 #   end
 #
 
-on_message %r{^/youtube http://youtu.be/(.*)$} do |youtube_id|
-  send_message "<iframe width='560' height='315' src='http://www.youtube.com/embed/#{youtube_id}' frameborder='0' allowfullscreen></iframe>"
-  stop_message_handling
-end
+#nothing in this file will work, now that we have multiple channels, sorry!!
 
-on_message %r{^/alert (.*)$} do |alert_message|
-  send_action 'show_alert', alert_message
-  stop_message_handling
-end
-
-on_message %r{^/earthquake$} do
-  send_status_message 'Someone has just caused an earthquake!'
-  send_action 'earthquake'
-  stop_message_handling
-end
+# on_message %r{^/youtube http://youtu.be/(.*)$} do |youtube_id|
+#   send_message "<iframe width='560' height='315' src='http://www.youtube.com/embed/#{youtube_id}' frameborder='0' allowfullscreen></iframe>"
+#   stop_message_handling
+# end
+# 
+# on_message %r{^/alert (.*)$} do |alert_message|
+#   send_action 'show_alert', alert_message
+#   stop_message_handling
+# end
+# 
+# on_message %r{^/earthquake$} do
+#   send_status_message 'Someone has just caused an earthquake!'
+#   send_action 'earthquake'
+#   stop_message_handling
+# end
